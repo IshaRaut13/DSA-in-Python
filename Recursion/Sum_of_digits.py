7e3635789770f0c795d23e170dbcc4504eb2f343
IshaRaut13 <139954745+IshaRaut13@users.noreply.github.com>
2025-12-11 17:48:45 +0530

    adding the numbers

--- Recursion/Sum_of_digits.py
+++ Recursion/Sum_of_digits.py
@@ -3,7 +3,7 @@ def sum_of_digits(n):
         return n  # if its single number then print as it is
     else:
         # if its multiple numbers then sum all the numbers
-        return n % 10 + sum_of_digits(n / 10)
+        return n % 10 + sum_of_digits(n // 10)
 
 
 a = int(input("Enter the numbers"))
