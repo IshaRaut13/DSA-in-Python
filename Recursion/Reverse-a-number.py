a013dd2d34bf1d07bf2d9dcd3166103122b55b2f
IshaRaut13 <139954745+IshaRaut13@users.noreply.github.com>
2025-12-12 20:05:39 +0530

    Reversing a number

---
+++ Recursion/Reverse-a-number.py
@@ -0,0 +1,9 @@
+def reverse(n, rev=0):
+    if n == 0:
+        return rev
+    else:
+        return reverse(n // 10, rev * 10 + n % 10)
+
+
+a = int(input("Enter the number: "))
+print(reverse(a))
